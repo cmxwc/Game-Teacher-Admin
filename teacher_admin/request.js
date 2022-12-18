e676a091f7948abb1b9e6c5ab5f31fcd335c90a0
cmxwc <59865108+cmxwc@users.noreply.github.com>
2022-12-18 12:12:47 +0800

    first commit

---
+++ teacher_admin/request.js
@@ -0,0 +1,32 @@
+const url = "http://localhost:8000/"
+
+
+fetch(url+"get_userData_all")
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
+
+fetch('https://reqres.in/api/users/')
+  .then(response => {
+    if (response.ok){
+      console.log('SUCCESS')
+    } else {
+      console.log('Not Successful')
+    }
+  })
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
+
+fetch('https://reqres.in/api/users/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name: 'User 1'
+    })
+  }).then (response => {
+    return response.json()
+  })
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
