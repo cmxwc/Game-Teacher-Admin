336eb962a9cd35f6dce79dba684f5fa5b419b216
cmxwc <59865108+cmxwc@users.noreply.github.com>
2022-12-18 17:31:25 +0800

    added create question html css

--- request.js
+++ request.js
@@ -6,27 +6,3 @@ fetch(url+"get_userData_all")
   .then(data => console.log(data))
   .catch(error => console.error(error));
 
-fetch('https://reqres.in/api/users/')
-  .then(response => {
-    if (response.ok){
-      console.log('SUCCESS')
-    } else {
-      console.log('Not Successful')
-    }
-  })
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
-
-fetch('https://reqres.in/api/users/', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      name: 'User 1'
-    })
-  }).then (response => {
-    return response.json()
-  })
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
