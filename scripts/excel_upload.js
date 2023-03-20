b9f961edaa0d5c4c0e447be6730d560cf24461d9
cmxwc <59865108+cmxwc@users.noreply.github.com>
2023-03-21 00:31:23 +0800

    fix excel questions

--- scripts/excel_upload.js
+++ scripts/excel_upload.js
@@ -1,19 +1,24 @@
 import {http_url, fetchData, postData} from './request.js';
 
 class Question {
-    constructor(questionId, questionSubject, questionTopic, questionText, questionAnsIndex, questionAnsText, questionLearningObj) {
+    constructor(questionId, questionSubject, questionTopic, questionDifficulty, questionText, questionAns, option1, option2, option3, option4, questionLearningObj) {
         this.questionId = questionId;
         this.questionSubject = questionSubject;
         this.questionTopic = questionTopic;
+        this.questionDifficulty = questionDifficulty;
         this.questionText = questionText;
-        this.questionAnsIndex = questionAnsIndex;
-        this.questionAnsText = questionAnsText;
+        this.questionAns = questionAns;
+        this.option1 = option1
+        this.option2 = option2
+        this.option3 = option3
+        this.option4 = option4
         this.questionLearningObj = questionLearningObj;
     }
 }
 
 const excel_file = document.getElementById('excel_file');
-const submit = document.getElementById('create_question')
+const submit = document.getElementById('create-question');
+let sheet_data;
 
 excel_file.addEventListener('change', (event) => {
 
@@ -30,77 +35,91 @@ excel_file.addEventListener('change', (event) => {
 
     reader.readAsArrayBuffer(event.target.files[0]);
 
-    reader.onload = function(event){
-
+    reader.onload = function (event) {
         var data = new Uint8Array(reader.result);
-
         var work_book = XLSX.read(data, {type:'array'});
-
         var sheet_name = work_book.SheetNames;
-
-        var sheet_data = XLSX.utils.sheet_to_json(work_book.Sheets[sheet_name[0]], {header:1});
+        sheet_data = XLSX.utils.sheet_to_json(work_book.Sheets[sheet_name[0]], { header: 1 });
 
         if(sheet_data.length > 0)
         {
             var table_output = '<table class="table table-striped table-bordered">';
-
             for(var row = 0; row < sheet_data.length; row++)
             {
-
                 table_output += '<tr>';
-
                 for(var cell = 0; cell < sheet_data[row].length; cell++)
                 {
-
                     if(row == 0)
                     {
-
                         table_output += '<th>'+sheet_data[row][cell]+'</th>';
-
                     }
                     else
                     {
-
                         table_output += '<td>'+sheet_data[row][cell]+'</td>';
-
                     }
-
                 }
-
                 table_output += '</tr>';
-
             }
-
             table_output += '</table>';
-
             document.getElementById('excel_data').innerHTML = table_output;
         }
-
         excel_file.value = '';
-
     }
 
-    readXlsxFile(excel_file.files[0]).then(function (data) {
-        var i = 0;
-        var headers = [];
-        var json_object = [];
-
-        // assign headers into rows
-        data.map((row, index) => {
-            if (i == 0) {
-                headers = row;
-            }
-            if (i > 0) {
-                var temp = {};
-                for (var x = 0; x < row.length; x++) {
-                    temp[headers[x] = row[x]];
-                }
-                json_object.push(temp);
-            }
-            i++;
-        });
-        var questions = JSON.stringify(json_object, null, 2)
-        console.log(questions)
-    });
+     
 
 });
+
+
+submit.addEventListener("click", async(e) => {
+    e.preventDefault(); // prevent autosubmitting
+    var subject = document.getElementById("subject").value;
+    var questionList = parseExcelArray(sheet_data);
+
+    const response = await postData(http_url + "update_question_subject_bank?subject=" + subject, questionList);
+    console.log(response);
+    
+    // var username = document.getElementById("username").value;
+    // var password = document.getElementById("password").value;
+    // console.log("The username is " + username + " the password is " + password)
+    // let teacherLogin = new TeacherLogin(username, password)
+    // const response = await postData(http_url + "login_teacher", teacherLogin);
+    // if (response == "Successfully authenticated") {
+    //     localStorage.setItem("auth", 1);
+    //     window.location.assign("home3.html")
+    // }
+    // else {
+    //     validateText.style.display = 'block';
+    // }
+})
+
+
+function parseExcelArray(sheet_data) {
+    var questionList = [];
+        for(var row = 1; row < sheet_data.length; row++)
+        {
+            if (sheet_data[row].length > 0) {
+                var questionId = sheet_data[row][0];
+                var questionSubject = sheet_data[row][1];
+                var questionTopic = sheet_data[row][2];
+                var questionDifficulty = sheet_data[row][3];
+                var questionText = sheet_data[row][4];
+                var questionAns = sheet_data[row][5];
+                var option1 = sheet_data[row][6]
+                var option2 = sheet_data[row][7]
+                var option3 = sheet_data[row][8]
+                var option4 = sheet_data[row][9]
+                var questionLearningObj = sheet_data[row][10];
+                var question = new Question(questionId, questionSubject, questionTopic,
+                    questionDifficulty, questionText, questionAns,
+                    option1, option2, option3, option4, questionLearningObj);
+                console.log(question); 
+                questionList.push(question);
+                
+            }
+            
+        }
+    console.log("=====Question List======");
+    console.log(questionList);
+    return questionList;
+}
